32cdb42ff01e9f2b796a91de9a62bac3b5b60acc
federicoangioni <f.angioni@student.tudelft.nl>
2025-03-17 16:00:30 +0100

    working with gym and gymnasium

--- python/gym.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-print("Num GPUs Available: ", len(tf.config.list_physical_devices(‘GPU’)))
-
-if tf.config.list_physical_devices('GPU'):
-  print("TensorFlow **IS** using the GPU")
-else:
-  print("TensorFlow **IS NOT** using the GPU")
