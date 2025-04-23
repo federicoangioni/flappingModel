01fc2de0901f304b0ab13df5b1f0e50c415a8971
Federico Angioni <f.angioni@student.tudelft.nl>
2025-04-23 20:17:40 +0200

    figured out necessary flight data values for figure 15 of bio&bio

--- force_model.py
+++ force_model.py
@@ -3,11 +3,10 @@ import scipy.io
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 from scipy.signal import butter, filtfilt, savgol_filter
-from scipy.integrate import solve_ivp
 
 # %% Load the data
-i = 1  # Index for first experiment (Python uses 0-based indexing)
-
+i = 3  # Index for first experiment (Python uses 0-based indexing)
+j=0
 mat = scipy.io.loadmat('dataset_revision.mat', squeeze_me=True, struct_as_record=False)
 
 # Experimental setup
@@ -24,11 +23,14 @@ TIME = data.motion_tracking.TIME
 PITCHavg = data.motion_tracking.PITCH_avg
 VELxavg = data.motion_tracking.VEL_BODYx_filtered_avg
 DIHEDavg = data.motion_tracking.DIHEDRAL_avg
+
 ACCx = data.motion_tracking.ACC_BODYx_filtered
 VELzavg = data.motion_tracking.VEL_BODYz_filtered_avg
 ACCz=data.motion_tracking.ACC_BODYz_filtered
 OMyavg=data.motion_tracking.OMy_filtered_avg
 ALPHyavg=data.motion_tracking.ALPHy_filtered_avg
+VELz=data.motion_tracking.VEL_BODYz_filtered
+VELx = data.motion_tracking.VEL_BODYx_filtered
 
 # %% Parameters
 bx = 0.07220
@@ -74,8 +76,8 @@ f_theta = interp1d(TIME, theta_smooth, kind='cubic', fill_value="extrapolate")
 thetad_est = np.gradient(theta_smooth, dt)
 f_thetad = interp1d(TIME, thetad_est, kind='cubic', fill_value="extrapolate")
 
-f_u = interp1d(TIME, VELxavg, kind='cubic', fill_value="extrapolate")
-f_w = interp1d(TIME, VELzavg, kind='cubic', fill_value="extrapolate")
+f_u = interp1d(TIME, VELx, kind='cubic', fill_value="extrapolate")
+f_w = interp1d(TIME, VELz, kind='cubic', fill_value="extrapolate")
 
 # Interpolate u, w, fld, fldd from flight data
 u_vals = f_u(TIME)
@@ -85,7 +87,7 @@ fldd_vals = f_ldd(TIME)
 thetad_vals = f_thetad(TIME)
 theta_vals = f_theta(TIME)
 
-fx_model =  -np.sin(theta_vals) * g - 2 * bx / m * (u_vals - lz * theta_vals + fldd_vals)
+fx_model =  -np.sin(theta_vals) * g - 2*bx / m * (VELx[0] - lz * theta_vals + fldd_vals)
 
 T = 2*(c1*f + c2)
 
@@ -95,35 +97,44 @@ moment_model = (-(- 2*bx * lz * (u_vals - lz * thetad_vals + fldd_vals)
                    + 2*bz * fld_vals * (w_vals - fld_vals * thetad_vals)
                    - T * fld_vals) / Iyy)
 
-# Define least squares
-def optimize(params):
-    bx_, bz_, lz_ = params
+from scipy.optimize import least_squares
 
+# Define least squares
 # %% Plotting model behaviour
 plt.figure()
 
 plt.suptitle(f'Forces and moments model for {legend_list[i]}')
 plt.subplot(3, 1, 1)
-plt.plot(TIME, fx_model, linewidth=2, label='Model', linestyle='--')
+plt.plot(TIME, fx_model, linewidth=2, label='Model original', linestyle='--')
+# plt.plot(TIME, fx_model_fit, linewidth=2, label='Model fitted', linestyle='--')
 plt.plot(TIME, np.mean(ACCx, axis=0), label='Flight Data')
 plt.title(r'$F_x$ model')
-plt.ylabel(r'$\dot{u}$ ($rad/s$)')
+plt.ylabel(r'$\dot{u}$ ($m/s^2$)')
 plt.legend()
 plt.gca().set_xticklabels([])
 
 plt.subplot(3, 1, 2)
-plt.plot(TIME, fz_model, linewidth=2, label='Model', linestyle='--')
+# plt.plot(TIME, fz_model, linewidth=2, label='Model', linestyle='--')
 plt.plot(TIME, np.mean(ACCz, axis=0), label='Flight Data')
 plt.title(r'$F_z$ model')
-plt.ylabel(r'$\dot{w}$ ($rad/s$)')
+plt.ylabel(r'$\dot{w}$ ($m/s^2$)')
 plt.gca().set_xticklabels([])
 
 plt.subplot(3, 1, 3)
-plt.plot(TIME, moment_model, linewidth=2, label='Model', linestyle='--')
+# plt.plot(TIME, moment_model, linewidth=2, label='Model', linestyle='--')
 plt.plot(TIME, np.radians(ALPHyavg), label='Flight Data')
 plt.title(r'$M_y$ model')
 plt.xlabel('Time (s)')
 plt.ylabel(r'$\ddot{theta}$ ($rad/s^2$)')
 
 plt.tight_layout()
+plt.show()
+
+plt.figure()
+
+plt.suptitle('Fitted forces and moments model for {legend_list[i]}')
+
+
+plt.plot(TIME, VELx[0])
+plt.plot(TIME, fx_model)
 plt.show()
---
+++ forces.py
@@ -0,0 +1,101 @@
+import numpy as np
+import scipy
+from scipy.signal import butter, filtfilt
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
+
+# %% Define constants
+g = 9.81
+bx = 0.0722 # 4.21e-3 in paper
+lz = 80.1e-3
+m = 29.6e-3
+lw = 81e-3
+# %% Extract data from mat files
+Nexp=2
+nman=0
+
+# Figure 15 of Minimal Longitudinal uses experiment2, run 0
+# checked up to 79
+# 4, 13(?), 36 (almost sure), 54, 55(2, 4), 57(1)
+mat = scipy.io.loadmat('dataset_revision.mat', squeeze_me=True, struct_as_record=False)
+
+experiment_key = f"experiment{Nexp}"
+data = mat[experiment_key]
+
+# %% Extract necessary flight data
+# Optitrack time
+time = data.motion_tracking.TIME
+thetadd_opti = np.radians(data.motion_tracking.ALPHy[nman])
+thetadd_onboard = data.onboard.rates.ALPHy_IMU_filtered[nman]
+time_onboard_rates = data.onboard.rates.TIME_onboard_rates[nman]
+
+# Frequency, both onboard and interpolated + setpoints
+ff = data.onboard_interpolated.FREQright_wing_interp[nman]
+freq_right = data.onboard.frequency.FREQright_wing[nman]
+time_freq = data.onboard.frequency.TIME_onboard_freq[nman]
+CMDRight = data.onboard_interpolated.CMDright_motor_interp[nman]
+# CMDRight = data.onboard.angles_commands_setpoints.CMDthrottle_filtered[nman]
+
+# Accelerations, TODO what is actually DVEL?
+accx = data.motion_tracking.DVEL_BODYx_filtered[nman]
+accz = data.motion_tracking.DVEL_BODYz_filtered[nman]
+
+# TODO: figure out the setpoints
+
+# Onboard time
+time_onboard = data.onboard.angles_commands_setpoints.TIME_onboard[nman]
+# ff = np.mean(data.onboard.frequency.FREQright_wing[nman])
+
+dihedral = data.motion_tracking.DIHEDRAL[nman]
+
+
+# %% Necessary to the model
+
+pitch = np.radians(data.motion_tracking.PITCH[nman])
+velx = data.motion_tracking.VEL_BODYx_filtered[nman]
+
+
+# %% Butterworth filter
+fs = 1/np.mean(np.diff(time))
+fc = 5
+order = 4
+
+b, a = butter(order, fc / (fs/2))
+accz = filtfilt(b, a, accz)
+accx = filtfilt(b, a, accx)
+thetadd_onboard = filtfilt(b, a, thetadd_onboard)
+thetadd_opti = filtfilt(b, a, thetadd_opti)
+
+
+# %% Plotting
+plt.subplot(5, 1, 1)
+plt.plot(time, accx)
+plt.ylabel(r'$\dot{u}$ [m/$s^2$]')
+plt.xlim(-1.5, 2.5)
+plt.ylim(-20, 20)
+
+plt.subplot(5, 1, 2)
+plt.plot(time, accz)
+plt.xlim(-1.5, 2.5)
+plt.ylim(-10, 30)
+plt.ylabel(r'$\dot{w}$ [m/$s^2$]')
+
+plt.subplot(5, 1, 3)
+plt.plot(time, thetadd_opti)
+plt.ylabel(r'$\ddot{\theta}$ [rad/s$^2$]')
+plt.xlim(-1.5, 2.5)
+plt.ylim(-100, 100)
+
+plt.subplot(5, 1, 4)
+plt.plot(time, dihedral)
+plt.ylabel(r'$\gamma_2 [deg]$')
+plt.xlim(-1.5, 2.5)
+
+plt.subplot(5, 1, 5)
+plt.plot(time, ff)
+plt.plot(time, CMDRight/60)
+plt.ylabel(r'f [Hz]')
+plt.xlim(-1.5, 2.5)
+
+plt.tight_layout()
+plt.show()
