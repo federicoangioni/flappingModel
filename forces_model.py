33e79c51185ec1630b23d417f6ab0e6ed0e736d4
Federico Angioni <f.angioni@student.tudelft.nl>
2025-04-23 23:37:46 +0200

    finalise model with flight data input

--- forces_model.py
+++ forces_model.py
@@ -11,10 +11,15 @@ bx = 4.21e-3 # in paper
 lz = 80.1e-3
 m = 29.6e-3
 lw = 81e-3
+bz = 9.16e-4
+c1 = 0.0114
+c2 = -0.0449
+Iyy = 1.26e-4
+c_corr = 0.175
 # %% Extract data from mat files
 Nexp=2
 nman=0
-c_corr = 0.175
+
 # Figure 15 of Minimal Longitudinal uses experiment2, run 0
 # checked up to 79
 # 4, 13(?), 36 (almost sure), 54, 55(2, 4), 57(1)
@@ -88,46 +93,67 @@ t_dih, y_dih, _ = lsim(system, U=CMD_dihed, T=time)
 
 # correction on dihedral due to velocity
 dih_corr = -c_corr*velx + y_dih
+
 # %% Force modeling
 ld = lw*np.sin(dih_corr)
 ldd = np.gradient(ld)
 
-fx = -np.sin(pitch)*g - y_ff * bx / m * (velx - lz*omy + ldd) - omy*velz
+T = lambda f: 2*(c1*f + c2)
+fx = -np.sin(pitch)*g - ff * bx / m * (velx - lz*omy + ldd) - omy*velz
+fz = np.cos(pitch)*g - ff * bz / m * (velz - ld*omy) + omy*velx - T(ff)/m
+my = (-bx*ff * lz*(velx - lz*omy + ldd) + bz*y_ff*ld*(velz- ld*omy) - T(ff)*ld)/Iyy
 
 # %% Plotting
-plt.subplot(5, 1, 1)
-plt.plot(time, accx)
-plt.plot(time, fx)
-plt.ylabel(r'$\dot{u}$ [m/$s^2$]')
-plt.xlim(0.5, 3.5)
-plt.ylim(-20, 20)
 
-plt.subplot(5, 1, 2)
-plt.plot(time, accz)
-plt.xlim(0.5, 3.5)
-plt.ylim(-10, 30)
-plt.ylabel(r'$\dot{w}$ [m/$s^2$]')
+fig, axs = plt.subplots(5, 1)
 
-plt.subplot(5, 1, 3)
-plt.plot(time, thetadd_opti)
-plt.ylabel(r'$\ddot{\theta}$ [rad/s$^2$]')
-plt.xlim(0.5, 3.5)
-plt.ylim(-100, 100)
 
-plt.subplot(5, 1, 4)
-plt.plot(time, dihedral, linewidth=1.0, label='Flight data')
-plt.plot(t_dih, np.degrees(dih_corr), linestyle='--', color= 'black', label='Simulation')
-plt.plot(time, np.degrees(CMD_dihed), linestyle='--', color= 'darkkhaki', label='Setpoint')
-plt.ylabel(r'$\gamma_2 [deg]$')
-plt.xlim(0.5, 3.5)
+axs[0].plot(time, accx)
+axs[0].plot(time, fx, linestyle='--', color= 'black')
+axs[0].set_ylabel(r'$\dot{u}$ [m/$s^2$]')
+axs[0].set_xlim(0.5, 3.5)
+axs[0].set_ylim(-20, 20)
+axs[0].spines['top'].set_visible(False)
+axs[0].spines['right'].set_visible(False)
+axs[0].set_xticklabels([])
 
-plt.subplot(5, 1, 5)
-plt.plot(time, ff, linewidth=1.0, label='Flight data')
-plt.plot(t_ff, y_ff, linestyle='--', color='black', label='Simulation')
-plt.plot(time, CMDRight, color= 'darkkhaki', linestyle='--', label='Setpoint')
-plt.ylabel(r'f [Hz]')
-plt.xlim(0.5, 3.5)
-plt.ylim(5.0, 27)
+axs[1].plot(time, accz)
+axs[1].plot(time, fz, linestyle='--', color= 'black')
+axs[1].set_xlim(0.5, 3.5)
+axs[1].set_ylim(-10, 35)
+axs[1].set_ylabel(r'$\dot{w}$ [m/$s^2$]')
+axs[1].spines['top'].set_visible(False)
+axs[1].spines['right'].set_visible(False)
+axs[1].set_xticklabels([])
 
+axs[2].plot(time, thetadd_opti)
+axs[2].plot(time, -my, linestyle='--', color= 'black')
+axs[2].set_ylabel(r'$\ddot{\theta}$ [rad/s$^2$]')
+axs[2].set_xlim(0.5, 3.5)
+axs[2].set_ylim(-100, 100)
+axs[2].spines['top'].set_visible(False)
+axs[2].spines['right'].set_visible(False)
+axs[2].set_xticklabels([])
+
+axs[3].plot(time, dihedral, linewidth=1.0, label='Flight data')
+axs[3].plot(t_dih, np.degrees(dih_corr), linestyle='--', color= 'black', label='Simulation')
+axs[3].plot(time, np.degrees(CMD_dihed), linestyle='--', color= 'darkkhaki', label='Setpoint')
+axs[3].set_ylabel(r'$\gamma_2 [deg]$')
+axs[3].set_xlim(0.5, 3.5)
+axs[3].spines['top'].set_visible(False)
+axs[3].spines['right'].set_visible(False)
+axs[3].set_xticklabels([])
+
+axs[4].plot(time, ff, linewidth=1.0, label='Flight data')
+axs[4].plot(t_ff, y_ff, linestyle='--', color='black', label='Simulation')
+axs[4].plot(time, CMDRight, color= 'darkkhaki', linestyle='--', label='Setpoint')
+axs[4].set_ylabel(r'f [Hz]')
+axs[4].set_xlabel(r'Time [s]')
+axs[4].set_xlim(0.5, 3.5)
+axs[4].set_ylim(5.0, 27)
+axs[4].spines['top'].set_visible(False)
+axs[4].spines['right'].set_visible(False)
+
+fig.align_ylabels(axs[:])
 plt.tight_layout()
 plt.show()
